0d3088186f517d7baa68122cb5e1a2439911b273
abidbagusk <abidbagudkurniawan@gmail.com>
2019-01-27 17:15:19 +0700

    Create function closeNav() to close sidenav

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ const sidenav = document.getElementById('mySidenav');
 const menu = document.getElementById('main');
 
 function openNav() {
+    // Sidenav will span 300px width
     sidenav.style.width = '300px';
+    // Navbar will also span 300px width
     menu.style.marginLeft = '300px';
+}
+
+function closeNav() {
+    // Sidenav will back to normal width and hide the sidebar
+    sidenav.style.width = '0';
+    // Navbar will also back to norma width
+    menu.style.marginLeft = '0';
 }
