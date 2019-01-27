70f603142620de5deace4e246bb05a242b60dbc3
abidbagusk <abidbagudkurniawan@gmail.com>
2019-01-27 17:09:59 +0700

    Create function to open sidenav

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const sidenav = document.getElementById('mySidenav');
+const menu = document.getElementById('main');
+
+function openNav() {
+    sidenav.style.width = '300px';
+    menu.style.marginLeft = '300px';
+}
